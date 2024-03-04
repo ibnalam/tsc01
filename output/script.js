611993cfb8df618204a2856250c382a937af757b
MD IBN ALAM <97562405+ibnalam@users.noreply.github.com>
2024-03-04 19:00:20 +0600

    working with classes

--- output/script.js
+++ output/script.js
@@ -9,13 +9,31 @@
 // ) => {
 //     console.log(`User id is ${id} name is ${user.name}, and age is ${user.age}` );
 // }
-let calculation;
-calculation = (a, b, c) => {
-    if (c == "add") {
-        return a + b;
+// let calculation: (a: number, b: number, c: string) => number;
+// calculation = (a: number, b: number, c: string) => {
+//     if(c == "add"){
+//         return a + b
+//     }else {
+//         return a - b
+//     }
+// }
+// console.log(calculation(5, 6, "minus"));
+// warking with classes  //////////
+class player {
+    constructor(n, a, c) {
+        this.name = n;
+        this.age = a;
+        this.country = c;
     }
-    else {
-        return a - b;
+    play() {
+        console.log(`${this.name} from ${this.country} is playing!`);
     }
-};
-console.log(calculation(5, 6, "minus"));
+}
+const arafat = new player("arafat", 40, "bangladesh");
+const ibne = new player("ibne", 30, "bangladesh");
+const mejan = new player("mejan", 20, "bangladesh");
+console.log(ibne.name);
+const players = [];
+players.push(ibne);
+players.push(arafat);
+players.push(mejan);
--- src/script.ts
+++ src/script.ts
@@ -13,15 +13,52 @@
 // }
 
 
-let calculation: (a: number, b: number, c: string) => number;
+// let calculation: (a: number, b: number, c: string) => number;
 
-calculation = (a: number, b: number, c: string) => {
-    if(c == "add"){
-        return a + b
-    }else {
-        return a - b
+// calculation = (a: number, b: number, c: string) => {
+//     if(c == "add"){
+//         return a + b
+//     }else {
+//         return a - b
+//     }
+// }
+
+// console.log(calculation(5, 6, "minus"));
+
+
+
+// warking with classes  //////////
+
+class player {
+    name: string;
+    age: number;
+    country: string;
+    
+
+    constructor(n: string, a: number, c: string){
+        this.name = n;
+        this.age = a;
+        this.country = c;
+    }
+    play(){
+        console.log(`${this.name} from ${this.country} is playing!`);
     }
 }
 
-console.log(calculation(5, 6, "minus"));
+const arafat = new player("arafat", 40, "bangladesh")
+const ibne = new player("ibne", 30, "bangladesh")
+const mejan = new player("mejan", 20, "bangladesh")
+
+console.log(ibne.name);
+
+
+const players: player[] = [];
+
+players.push(ibne)
+players.push(arafat)
+players.push(mejan)
+
+
+
+
 
